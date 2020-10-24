1f78a860780ab7bd1b09f5fbf9f67ddc14d59641
chrisandrews7 <chrisandrews7@users.noreply.github.com>
2020-10-24 18:39:15 +0100

    house robber using dp

--- house_robber.go
+++ house_robber.go
@@ -1,32 +1,24 @@
 package main
 
-func rob(nums []int, current int, cache map[int]int) int {
-	switch current {
-	case len(nums) - 1:
-		return nums[current]
-	case len(nums):
+func houseRobber(nums []int) int {
+	if len(nums) < 1 {
 		return 0
 	}
 
-	if value, ok := cache[current]; ok {
-		return value
-	}
-
-	twoDown := nums[current]
-	if len(nums)+2 > current {
-		twoDown += rob(nums, current+2, cache)
-	}
-
-	if nextDoor := rob(nums, current+1, cache); nextDoor > twoDown {
-		cache[current] = nextDoor
-		return nextDoor
-	}
-
-	cache[current] = twoDown
-	return twoDown
-}
-
-func houseRobber(nums []int) int {
 	cache := make(map[int]int)
-	return rob(nums, 0, cache)
+	cache[0] = nums[0]
+
+	for i := 1; i < len(nums); i++ {
+		nextDoor := cache[i-1]
+		twoDown := cache[i-2] + nums[i]
+
+		if nextDoor > twoDown {
+			cache[i] = nextDoor
+			continue
+		}
+
+		cache[i] = twoDown
+	}
+
+	return cache[len(nums)-1]
 }
